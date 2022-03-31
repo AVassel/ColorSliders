e38c5a202a9c582e605799fba838e177c3f056b9
Anton Vassel <ant-vassel@ya.ru>
2022-03-31 15:57:24 +0300

    Working variant of homework.

--- ColorSliders/ViewController.swift
+++ ColorSliders/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var canvasView: UIView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        canvasView.layer.cornerRadius = 25
+        
+        //Label setup
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        paintCanvas()
     }
 
+    @IBAction func redColorAction() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        paintCanvas()
+    }
 
+    @IBAction func greenColorAction() {
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        paintCanvas()
+    }
+    
+    @IBAction func blueColorAction() {
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        paintCanvas()
+    }
+    private func paintCanvas() {
+        canvasView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
 }
 
